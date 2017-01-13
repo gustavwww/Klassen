315dbec3065186a0e668d228ee412d2663de1321
Gustav Wadström <gustavwadstrom@Gustavs-iMac.local>
2017-01-13 23:20:58 +0100

    Firebase Base Pod installed

---
+++ Pods/Target Support Files/Pods-Klassen/Pods-Klassen-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Klassen : NSObject
+@end
+@implementation PodsDummy_Pods_Klassen
+@end
---
+++ Pods/Target Support Files/Pods-Klassen/Pods-Klassen-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_KlassenVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_KlassenVersionString[];
+
